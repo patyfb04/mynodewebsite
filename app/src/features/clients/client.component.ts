d0a77c735b3f0bbe59b820f7fa6a3e25b822d674
Patricia <patyfb04@gmail.com>
2022-06-13 16:23:32 -0400

    client forms

--- app/src/features/clients/client.component.ts
+++ app/src/features/clients/client.component.ts
@@ -12,25 +12,36 @@ export class ClientComponent implements OnInit {
   public dataSource: Observable<any>;
   public clientId: any;
   public displayedColumns: string[] = ['name', 'email', 'active', 'id'];
-  
+  public display: boolean = false;
+  public isEdit: boolean = false;
+
   constructor(private activateRoute: ActivatedRoute, private clientService: ClientService) {
     this.dataSource = new Observable<any>();
   }
 
   public ngOnInit(): void {
-    this.clientService.getAll().subscribe((result : any) => {
+    this.clientService.getAll().subscribe((result: any) => {
       this.dataSource = result;
     })
 
-    this.activateRoute.params.subscribe((params : any)=> {
+    this.activateRoute.params.subscribe((params: any) => {
       if (params["id"] !== undefined) {
         this.clientId = params["id"]
       }
     })
   }
 
-  public edit(id: number) {
-    console.log(id)
+  public displayForm(eventName: any, id: any) {
+    this.display = !this.display;
+    this.isEdit = eventName == 'edit' ? true: false;
+  }
+
+  public create() {
+
+  }
+
+  public edit() {
+
   }
 
 }
