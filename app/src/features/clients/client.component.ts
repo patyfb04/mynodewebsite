e680176cd03f955230d5d3376a9d3338cb9326b0
Patricia <patyfb04@gmail.com>
2022-06-14 15:51:10 -0400

    sort table

--- app/src/features/clients/client.component.ts
+++ app/src/features/clients/client.component.ts
@@ -1,37 +1,64 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit,ChangeDetectorRef   } from '@angular/core';
 import { ClientService } from './client.service';
 import { Client } from './client';
 import { Observable } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
 import { FormControl, FormGroup } from '@angular/forms';
 import { AnyCatcher } from 'rxjs/internal/AnyCatcher';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
+import {MatTableDataSource, } from '@angular/material/table';
+import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'clients-view',
   templateUrl: './client.component.html',
   styleUrls: ['./client.component.sass']
 })
-export class ClientComponent implements OnInit {
-  public dataSource: Observable<any>;
-  public clientId: any;
+export class ClientComponent implements OnInit, AfterViewInit  {
+ // public dataSource: Observable<any>;
   public displayedColumns: string[] = ['name', 'email', 'active', 'id'];
   public display: boolean = false;
   public isEdit: boolean = false;
   public myForm: FormGroup;
   public selectedId : any;
+  public clients: Observable<any>;
+  public dataSource: MatTableDataSource<Client>;
 
-  constructor(private activateRoute: ActivatedRoute, private clientService: ClientService) {
-    this.dataSource = new Observable<any>();
+  @ViewChild(MatPaginator,  {static: false}) 
+  set paginator(value: MatPaginator) {
+    if (this.dataSource){
+      this.dataSource.paginator = value;
+    }
+  }
+
+  @ViewChild(MatSort,{static: false})
+  set sort(value: MatSort) {
+    if (this.dataSource){
+      this.dataSource.sort = value;
+    }
+  }
+
+  constructor(private activateRoute: ActivatedRoute, 
+              private clientService: ClientService) {
+
+    this.clients = new Observable<any>();
+    this.dataSource = new MatTableDataSource<Client>();
 
     this.myForm = new FormGroup({
       name: new FormControl(''),
       email: new FormControl(''),
       active: new FormControl(false)
     });
+
   }
 
   public ngOnInit(): void {
     this.loadData();
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
   }
 
   public displayForm(eventName: any, id: any) {
@@ -96,7 +123,7 @@ export class ClientComponent implements OnInit {
 
   public loadData() {
     this.clientService.getAll().subscribe((result: any) => {
-      this.dataSource = result;
+       this.dataSource.data = result;
     })
   }
 }
