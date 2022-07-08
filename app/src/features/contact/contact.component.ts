ebc0337c3a48b6e51a58bf1d58b0141ffe010058
Patricia <patyfb04@gmail.com>
2022-07-08 11:26:18 -0400

    contact

--- app/src/features/contact/contact.component.ts
+++ app/src/features/contact/contact.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ContactService } from './contact.service';
 import { Observable } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
+import { Contact } from './contact';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'contact-view',
@@ -9,17 +11,59 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./contact.component.sass']
 })
 export class ContactComponent implements OnInit {
-  public contactDescription: Observable<any>;
-  public userId: any;
+
+  public contactModel: Contact;
+  public myForm: FormGroup;
+  public submitResult: string = '';
 
   constructor(private activateRoute: ActivatedRoute, private contactService: ContactService) {
-    this.contactDescription = new Observable<any>();
+ 
+    this.myForm = new FormGroup({
+      description: new FormControl(''),
+      email: new FormControl('')
+    });
+    
+    this.initForm();
   }
 
   public ngOnInit(): void {
-    this.contactService.getAll().subscribe((result) => {
-      this.contactDescription = result[0];
+    this.contactService.getAll().subscribe((result: any) => {
+      this.contactModel = result[0];
     })
-
   }
+
+  onSubmit(form: FormGroup) {
+    this.submitResult = '';
+    const contact = new Contact(this.contactModel.id ? this.contactModel.id : 0, form.value.description, form.value.email);
+    this.update(contact);
+  }
+
+  public update(contact: Contact) {
+    this.contactService.update(contact).subscribe((result1: any) => {
+      this.submitResult = 'Contact info updated sucessfully!';
+      this.loadData();
+    });
+  }
+
+  public initForm() {
+    this.contactService.getAll().subscribe((result: any) => {
+      if (result.length > 0) {
+        this.myForm.patchValue({
+          description: result[0].description,
+          email: result[0].email
+        });
+      }
+    })
+  }
+
+  public loadData() {
+    this.contactService.getAll().subscribe((result: Contact[]) => {
+     this.contactModel = result[0];
+    })
+  }
+
+  public closeNotification(){
+    this.submitResult = ''
+  }
+
 }
