bd3ee4faf88f35faa753578cc4b284ad6986de2d
Patricia Braga <pbraga@wandrian.com>
2023-08-01 22:40:44 -0400

    test1

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -18,7 +18,7 @@ import { BookDeliverableComponent } from './../features/bookDeliverables/bookDel
 import { ArtworkComponent } from './../features/artworks/artwork.component';
 import { ServiceComponent } from './../features/service/service.component';
 import { ContactComponent } from './../features/contact/contact.component';
-import { TestimonialComponent } from './../features/testimonials/testimonial.component';
+// import { TestimonialComponent } from './../features/testimonials/testimonial.component';
 
 import { AppService } from './app.service';
 import { UserService } from './../features/users/user.service';
