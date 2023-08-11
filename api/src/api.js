6ce21c67c6b6a3b19e1897f688f217748707a9bb
Patricia Braga <pbraga@wandrian.com>
2023-08-11 13:16:14 -0400

    FIX

--- api/src/api.js
+++ api/src/api.js
@@ -5,6 +5,8 @@ var cors = require('cors')
 const path = require('path')
 const app = express()
 // app.use(express.static('public'))
+
+console.log('PATH=>',path.join(__dirname, 'images'))
 app.use('/images', express.static(path.join(__dirname, 'images')))
 
 app.use(cors())
